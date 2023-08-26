52be005dbee2b3309385e3b44b67f9104db2a1a2
asdfgdghfjj <katakiyaharshl001@gmail.com>
2023-08-26 11:08:51 +0530

    fix

--- server/db/connection.js
+++ server/db/connection.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const DB = process.env.DATABASE;
 
-mongoose.connect(DB,{
+mongoose.connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then( () => {
+}).then(() => {
     console.log('Connected to MongoDB');
-}).catch( (err) => {
+}).catch((err) => {
     console.log('Error in connecting to MongoDB');
-    console.log("Error : " ,err)
+    console.log("Error : ", err)
 });
